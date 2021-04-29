2c86f829e444368bd8ecc85d7a8e51da30267e1d
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2021-04-29 17:58:06 -0300

    Changes
    Scripts y escenario LVL 1

---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    //[SerializeField] private float doorOffsetX = -3f;
+    //[SerializeField] private float doorOffsetY = 0f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Vector3 moveDirection;
+
+    //Esto es lo que pasa cuando el player interactua con las llaves del nivel.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+        //Destroy(door.gameObject); La idea original era que se destruyan las puertas.
+        door.transform.position += moveDirection * speed * Time.deltaTime;
+    }
+}
---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private string playerName = "Default";
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float zRotation;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+    }
+}
